306b963fbc19d110120ca305a295d33cf2578a8d
Aurélien Mino <aurelien.mino@gmail.com>
2020-01-26 17:57:41 +0100

    NPM: use per user global packages

--- pkgs/fish.nix
+++ pkgs/fish.nix
@@ -5,5 +5,16 @@
   # fish install --noninteractive
   # checkout git@github.com:FaustXVI/omf-config in .config/omf
   # run omf install
-  xdg.configFile."fish/functions/fish_user_key_bindings.fish".source = ./key_bindings.fish ;
+  xdg.configFile."fish/functions/fish_user_key_bindings.fish".source = ./key_bindings.fish;
+
+  programs.fish = {
+    shellInit = ''
+      set NPM_PACKAGES "$HOME/.npm-packages"
+      if not test -e $NPM_PACKAGES 
+        mkdir $NPM_PACKAGES 
+      end
+      set PATH $NPM_PACKAGES/bin $PATH
+      set MANPATH $NPM_PACKAGES/share/man $MANPATH
+    '';
+  };
 }
