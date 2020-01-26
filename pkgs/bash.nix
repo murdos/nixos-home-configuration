224b0d4f8b21d9a05610ae9d1bbe86e2f48525fc
Aurélien Mino <aurelien.mino@gmail.com>
2020-01-26 23:00:36 +0100

    Npm global packages: fix bash config

--- pkgs/bash.nix
+++ pkgs/bash.nix
@@ -18,11 +18,8 @@
       ];
       initExtra = ''
         eval "$(direnv hook bash)"
-        NPM_PACKAGES="${HOME}/.npm-packages"
+        NPM_PACKAGES="$HOME/.npm-packages"
         export PATH="$PATH:$NPM_PACKAGES/bin"
-        # Preserve MANPATH if you already defined it somewhere in your config.
-        # Otherwise, fall back to `manpath` so we can inherit from `/etc/manpath`.
-        export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
       '';
     };
   };
