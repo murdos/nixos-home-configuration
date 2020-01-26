55d37a58b349233a14f39d89c4851da785fb3119
Aurélien Mino <aurelien.mino@gmail.com>
2020-01-26 17:50:57 +0100

    Move extra config to specific config keys when possible

--- pkgs/bash.nix
+++ pkgs/bash.nix
@@ -2,31 +2,21 @@
   programs = {
     bash = {
       enable = true;
-      bashrcExtra = ''
-# If not running interactively, don't do anything
-[ -z "$PS1" ] && return
-
-# append to the history file, don't overwrite it
-shopt -s histappend
-
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
-shopt -s checkwinsize
-
-alias ls='ls --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
-
-# some more ls aliases
-alias ll='ls -l'
-alias la='ls -A'
-alias l='ls -CF'
-
-if [ -f ~/.bashrc_for_profile ]; then
-    . ~/.bashrc_for_profile
-fi
-
+      shellAliases = {
+        ls = "ls --color=auto";
+        ll = "ls -l";
+        la = "ls -A";
+        l = "ls -CF";
+        vi = "vim";
+        grep = "grep --color=auto";
+        fgrep = "fgrep --color=auto";
+        egrep = "egrep --color=auto";
+      };
+      shellOptions = [
+        "histappend"
+        "checkwinsize"
+      ];
+      initExtra = ''
 eval "$(direnv hook bash)"
       '';
     };
--- pkgs/vim.nix
+++ pkgs/vim.nix
@@ -1,16 +1,19 @@
 {
   programs.vim = {
     enable = true;
+    settings = {
+      # Spaces instead of tabs
+      expandtab = true;
+      shiftwidth = 4;
+      tabstop = 4;
+      history = 50;
+      ignorecase = true;
+      number = true;
+    };
     extraConfig = ''
       filetype indent on
       " allow backspacing over everything in insert mode
       set backspace=indent,eol,start
-      " I like 4 spaces for indenting
-      set shiftwidth=4
-      " I like 4 stops
-      set tabstop=4
-      " Spaces instead of tabs
-      set expandtab
       " Always  set auto indenting on
       set autoindent
       " select when using the mouse
@@ -18,16 +21,12 @@
       " do not keep a backup files
       set nobackup
       set nowritebackup
-      " keep 50 lines of command line history
-      set history=50
       " show the cursor position all the time
       set ruler
       " show (partial) commands
       set showcmd
       " do incremental searches (annoying but handy);
       set incsearch
-      " Set ignorecase on
-      set ignorecase
       " Set status line
       set statusline=[%02n]\ %f\ %(\[%M%R%H]%)%=\ %4l,%02c%2V\ %P%*
       " Always display a status line at the bottom of the window
@@ -36,7 +35,6 @@
       set showmatch
       syntax on
       set hlsearch
-      set number
       '';
   };
 }
