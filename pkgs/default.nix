547309cc687cd9f00b086a4f89f093cb7893b305
Aurélien Mino <aurelien.mino@gmail.com>
2020-01-28 23:11:46 +0100

    Firefox custom settings

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./git.nix
     ./bash.nix
+    ./firefox.nix
     ./i3.nix
     ./vim.nix
     ./fish.nix
---
+++ pkgs/firefox.nix
@@ -0,0 +1,11 @@
+{
+  programs.firefox = {
+    profiles."default" = {
+      path = "haf6sy71.default";
+      isDefault = true;
+      settings = {
+        "browser.urlbar.clickSelectsAll" = true;
+      };
+    };
+  };
+}
