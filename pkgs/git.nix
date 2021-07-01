637597bfdae0b70de75457ff29a4bc42af13d930
Aurélien Mino <aurelien.mino@gmail.com>
2021-07-01 21:41:08 +0200

    Git: configure 'cache' as default credential helper

--- pkgs/git.nix
+++ pkgs/git.nix
@@ -11,6 +11,9 @@
       branch-clean = "!git branch --merged | grep -v master | xargs -n 1 git branch -d";
       next = "!git checkout `git rev-list HEAD..demo-end | tail -1`";
     };
+    extraConfig = {
+      credential.helper = "cache";
+    };
     includes = [{
       condition = "gitdir:~/git/enedis-six/";
       contents = {
