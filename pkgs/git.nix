fb1863770d2366b43a87773cf6586d99c1e5da96
Aurélien Mino <aurelien.mino@gmail.com>
2023-08-31 23:24:35 +0200

    Enable difftastic with git

--- pkgs/git.nix
+++ pkgs/git.nix
@@ -11,6 +11,7 @@
       branch-clean = "!git branch --merged | grep -v master | xargs -n 1 git branch -d";
       next = "!git checkout `git rev-list HEAD..demo-end | tail -1`";
     };
+    difftastic.enable = true;
     extraConfig = {
       credential.helper = "cache";
     };
