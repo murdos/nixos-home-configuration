11ad08f7ccedd29dd4c61514b38abdd9c6641213
Aurélien Mino <aurelien.mino@gmail.com>
2024-04-15 21:45:05 +0200

    chore(cypress): use newer version from auto-update PR

--- dev.nix
+++ dev.nix
@@ -26,6 +26,19 @@ with pkgs; {
     CYPRESS_RUN_BINARY = "${pkgs.cypress}/bin/Cypress";
   };
 
+  nixpkgs.overlays = [
+    (self: super: {
+      # https://github.com/NixOS/nixpkgs/pull/258854
+      cypress = (import
+        (builtins.fetchTarball {
+          url =
+            "https://github.com/r-ryantm/nixpkgs/archive/b60070f1795d93b34e59fa6d693e26e8575ab259.tar.gz";
+          sha256 = "1604cy5r0vc7c49bzw014zxc182xp1akpz964070zzqm7sb7hyjm";
+        })
+        { }).cypress;
+    })
+  ];
+
   home.packages = [
     jetbrains.idea-ultimate
     dbeaver
