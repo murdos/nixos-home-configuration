bf446241c24a744361f9b43c15e725ced53bf375
Aurélien Mino <aurelien.mino@gmail.com>
2024-06-13 18:37:35 +0200

    feat(gaming): Switch tools and emulator

--- gaming.nix
+++ gaming.nix
@@ -2,6 +2,8 @@
 with pkgs; {
   home.packages = [
     steam
+    nsz
+    ryujinx
     joycond # userspace daemon to combine joy-cons from the hid-nintendo kernel driver
     joycond-cemuhook # support for cemuhook's UDP protocol for joycond devices
   ];
