a2308a989b9d8d07f00749f9fbba58bd9df12459
Aurélien Mino <aurelien.mino@gmail.com>
2024-06-13 18:36:49 +0200

    feat(gaming): support for Switch game controller

--- gaming.nix
+++ gaming.nix
@@ -2,5 +2,7 @@
 with pkgs; {
   home.packages = [
     steam
+    joycond # userspace daemon to combine joy-cons from the hid-nintendo kernel driver
+    joycond-cemuhook # support for cemuhook's UDP protocol for joycond devices
   ];
 }
