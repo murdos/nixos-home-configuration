98481bdaafccd89f246471c1b33ac8cd78295ea5
Aurélien Mino <aurelien.mino@gmail.com>
2024-08-20 17:04:02 +0200

    feat(desktop): enabled synology-drive-client

--- desktop.nix
+++ desktop.nix
@@ -14,6 +14,7 @@ with pkgs; {
     chromaprint
     keepassxc
     protonvpn-gui
+    synology-drive-client
     pdftk # for manipulating pdfs
     xournalpp # for annotating pdfs
     handbrake # for converting videos
