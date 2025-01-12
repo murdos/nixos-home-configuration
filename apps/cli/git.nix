a4d3e4d14dad79f27aa7ca17c3fb6ba881f14a5b
Aurélien Mino <aurelien.mino@gmail.com>
2025-01-12 11:52:49 +0100

    feat(git): replace difftastic by delta and adjust push/pull configuration

--- apps/cli/git.nix
+++ apps/cli/git.nix
@@ -11,10 +11,17 @@
       branch-clean = "!git branch --merged | grep -v master | xargs -n 1 git branch -d";
       next = "!git checkout `git rev-list HEAD..demo-end | tail -1`";
     };
-    difftastic.enable = true;
+    delta = {
+      enable = true;
+      options = {
+        line-numbers = true;
+      };
+    };
     extraConfig = {
       credential.helper = "cache --timeout 3600";
       init.defaultBranch = "main";
+      push.default = "current";
+      pull.rebase = "merges";
     };
     includes = [{
       condition = "gitdir:~/git/enedis-*/";
