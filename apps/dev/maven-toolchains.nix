73f2f3e025954c3faeb959538023b6c3dffb19c9
Aurélien Mino <aurelien.mino@gmail.com>
2025-01-12 11:22:32 +0100

    dev(maven): dynamically generate maven toolchains configuration with all LTS jdks

---
+++ apps/dev/maven-toolchains.nix
@@ -0,0 +1,54 @@
+{ pkgs, ... }:
+
+with pkgs; {
+  home.file.".m2/toolchains.xml".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <toolchains>
+      <toolchain>
+        <type>jdk</type>
+        <provides>
+          <version>1.8</version>
+        </provides>
+        <configuration>
+          <jdkHome>${pkgs.openjdk8}</jdkHome>
+        </configuration>
+      </toolchain>
+      <toolchain>
+        <type>jdk</type>
+        <provides>
+          <version>11</version>
+        </provides>
+        <configuration>
+          <jdkHome>${pkgs.openjdk11}</jdkHome>
+        </configuration>
+      </toolchain>
+      <toolchain>
+        <type>jdk</type>
+        <provides>
+          <version>17</version>
+        </provides>
+        <configuration>
+          <jdkHome>${pkgs.openjdk17}</jdkHome>
+        </configuration>
+      </toolchain>
+      <toolchain>
+        <type>jdk</type>
+        <provides>
+          <version>21</version>
+        </provides>
+        <configuration>
+          <jdkHome>${pkgs.openjdk21}</jdkHome>
+        </configuration>
+      </toolchain>
+      <toolchain>
+        <type>jdk</type>
+        <provides>
+          <version>23</version>
+        </provides>
+        <configuration>
+          <jdkHome>${pkgs.openjdk23}</jdkHome>
+        </configuration>
+      </toolchain>
+    </toolchains>
+   '';
+}
