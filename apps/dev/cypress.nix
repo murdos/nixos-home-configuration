0593908f3565a3b964f271271825fce2f13c1b07
Aurélien Mino <aurelien.mino@gmail.com>
2025-01-15 00:13:58 +0100

    fix(dev): set required value for CYPRESS_INSTALL_BINARY

--- apps/dev/cypress.nix
+++ apps/dev/cypress.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 with pkgs; {
   home.sessionVariables = {
-    CYPRESS_INSTALL_BINARY = "1";
+    CYPRESS_INSTALL_BINARY = "0";
     CYPRESS_RUN_BINARY = "${pkgs.cypress}/bin/Cypress";
   };
   home.packages = [
