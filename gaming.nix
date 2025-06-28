ab15d06540efc24a812df4044d428824a11a912f
Aurélien Mino <aurelien.mino@gmail.com>
2025-06-28 13:48:09 +0200

    feat(gaming): add Heroic, a games launcher for GOG and Epic Games

--- gaming.nix
+++ gaming.nix
@@ -2,6 +2,7 @@
 with pkgs; {
   home.packages = [
     steam
+    heroic # Heroic Games Launcher, an open-source GOG and Epic Games Store client
     nsz
     ryujinx
     joycond # userspace daemon to combine joy-cons from the hid-nintendo kernel driver
