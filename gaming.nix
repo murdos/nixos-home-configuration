1432f39ac83d2d7e2cab514d983afdab23dd4559
Aurélien Mino <aurelien.mino@gmail.com>
2025-08-11 21:31:08 +0200

    chore(gaming): replace unmaintained ryujinx by ryubing

--- gaming.nix
+++ gaming.nix
@@ -4,7 +4,7 @@ with pkgs; {
     steam
     heroic # Heroic Games Launcher, an open-source GOG and Epic Games Store client
     nsz
-    ryujinx
+    ryubing
     joycond # userspace daemon to combine joy-cons from the hid-nintendo kernel driver
     joycond-cemuhook # support for cemuhook's UDP protocol for joycond devices
   ];
