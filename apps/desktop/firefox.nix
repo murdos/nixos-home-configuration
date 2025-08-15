1ed2bc6b6d109f664cc91c413afb367a96416c6e
Aurélien Mino <aurelien.mino@gmail.com>
2025-08-15 22:48:27 +0200

    feat(desktop): configure language packs for firefox

--- apps/desktop/firefox.nix
+++ apps/desktop/firefox.nix
@@ -8,6 +8,7 @@ with pkgs; {
         pkgs.fx-cast-bridge
       ];
     };
+    languagePacks = [ "fr-FR" "en-US" ];
     profiles."default" = {
       path = "haf6sy71.default";
       isDefault = true;
