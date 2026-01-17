c20f385e7602915c2a05d47482d4db19e1bb7dbf
Aurélien Mino <aurelien.mino@gmail.com>
2026-01-17 10:30:09 +0100

    feat(gaming): add highscore, a multi-emulator frontend

--- gaming.nix
+++ gaming.nix
@@ -4,6 +4,10 @@ with pkgs; {
     steam
     heroic # Heroic Games Launcher, an open-source GOG and Epic Games Store client
     nsz
+    highscore
+    highscore-mednafen
+    highscore-mupen64plus
+    highscore-bsnes
     ryubing
     joycond # userspace daemon to combine joy-cons from the hid-nintendo kernel driver
     joycond-cemuhook # support for cemuhook's UDP protocol for joycond devices
