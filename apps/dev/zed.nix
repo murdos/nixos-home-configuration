441d4b641c6840be8b4673a1c767d4c4dbd7fc2d
Aurélien Mino <aurelien.mino@gmail.com>
2026-02-06 22:37:08 +0100

    feat(dev): add zed

---
+++ apps/dev/zed.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+with pkgs; {
+  programs.zed-editor = {
+    enable = true;
+    extensions = [
+      "java"
+      "material-icon-theme"
+      "nix"
+      "proto"
+    ];
+    extraPackages = [
+      nixd
+    ];
+    userSettings = {
+      base_keymap = "JetBrains";
+      icon_theme = {
+        mode = "dark";
+        light = "Zed (Default)";
+        dark = "Material Icon Theme";
+      };
+      theme = {
+        mode = "dark";
+        light = "One Light";
+        dark = "One Dark";
+      };
+    };
+  };
+}
