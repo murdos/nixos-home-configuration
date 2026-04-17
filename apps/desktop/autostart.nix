d6b54f43b55427853f1a728863ccae14afaaea62
Aurélien Mino <aurelien.mino@gmail.com>
2026-04-17 06:30:35 +0200

    feat(autostart): conditionally install autostart entries for applications A desktop file won't be installed if the package is not enabled

--- apps/desktop/autostart.nix
+++ apps/desktop/autostart.nix
@@ -1,29 +1,35 @@
 {
+  config,
   pkgs,
   lib,
   ...
 }: let
-  inherit (lib.modules) mkMerge;
+  inherit (lib.modules) mkMerge mkIf;
 
   mapAutostart = {
     pkg,
     desktopFile,
-  }: {
-    xdg.configFile."autostart/${desktopFile}".source = "${pkg}/share/applications/${desktopFile}";
-  };
+    condition ? true,
+  }:
+    mkIf condition {
+      xdg.configFile."autostart/${desktopFile}".source = "${pkg}/share/applications/${desktopFile}";
+    };
 
   autostarts = [
     {
-      pkg = pkgs.firefox;
+      pkg = config.programs.firefox.package;
       desktopFile = "firefox.desktop";
+      condition = config.programs.firefox.enable;
     }
     {
       pkg = pkgs.synology-drive-client;
       desktopFile = "synology-drive.desktop";
+      condition = lib.elem pkgs.synology-drive-client config.home.packages;
     }
     {
       pkg = pkgs.localsend;
       desktopFile = "LocalSend.desktop";
+      condition = lib.elem pkgs.localsend config.home.packages;
     }
   ];
 in
