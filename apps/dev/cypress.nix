a8620f1d6071bc9ccb5453b5b9afd9bff903f7d2
Aurélien Mino <aurelien.mino@gmail.com>
2026-05-31 21:16:51 +0200

    chore(dev): update cypress using an overlay since version in nixpkgs is outdated

--- apps/dev/cypress.nix
+++ apps/dev/cypress.nix
@@ -3,9 +3,19 @@ with pkgs;
 {
   home.sessionVariables = {
     CYPRESS_INSTALL_BINARY = "0";
-    CYPRESS_RUN_BINARY = "${pkgs.cypress}/bin/Cypress";
+    CYPRESS_RUN_BINARY = "${pkgs.cypress}/bin/cypress";
   };
-  home.packages = [
-    cypress
+
+  nixpkgs.overlays = [
+    (self: super: {
+      # https://github.com/NixOS/nixpkgs/pull/437895
+      cypress = (import
+        (builtins.fetchTarball {
+          url =
+            "https://github.com/r-ryantm/nixpkgs/archive/04a75c371c0f39268b59dd28b5b40c5fd39db951.tar.gz";
+          sha256 = "1i565l37q0wrm2jiq88p4c36q5kw5di4ja2rlqi4k33hffn7nr67";
+        })
+        { }).cypress;
+    })
   ];
 }
